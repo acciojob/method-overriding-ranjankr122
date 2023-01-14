2de5f73a2f7b01a737afb05979938dc9bfa42c98
Ranjan <rkram122@gmail.com>
2023-01-14 12:55:15 +0530

    Asignment1-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B a=new B();
-        a.meth();
+      //  B a=new B();
+        //a.meth();
         B obj=new B();
+        obj.meth();
         obj.meth();
     }
   
